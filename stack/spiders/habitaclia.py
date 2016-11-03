83387773f303acde002fee577c411b419e62c0aa
Sergio <sergio.ramiro87@gmail.com>
2016-11-03 17:15:02 +0000

    Añadido tratamiento de habitaclia

--- stack/spiders/habitaclia.py
+++ stack/spiders/habitaclia.py
@@ -13,30 +13,19 @@ class VibboCrawlerSpider(CrawlSpider):
 
     rules = (
         #Rule (LinkExtractor(restrict_xpaths='//*[@class="siguiente"]'), process_links='link_filtering', follow= True),
-        Rule (LinkExtractor(restrict_xpaths='//*[@class="enlista"]/li//h3/a'), callback = 'parse_items'),
+        Rule (LinkExtractor(restrict_xpaths='//*[@id="listaAds"]/ul/li//*[@class="datos"]/h3/a'), callback = 'parse_items'),
     )
     
-
-    #def link_filtering(self, links):
-    #    ret = []
-    #    for link in links:
-    #        print(link)
-    #        print('***************')
-    #        i=1
-    #        sep='.htm'
-    #        #link.url = start_urls.split(sep, 1)[0] + str(i) + sep
-    #        i=i+1
-    #        print(link)
-    #        print('++++++++++++++++++')
-    #    yield links
-        
     def parse_items(self, response):
         item = StackItem()
         item["company"]     = 'habitaclia'
         item["url"]         = response.url
-        item["title"]       = self.format_xpath(response, '//*[@id="inificha"]/div/div[2]/div[1]/h1/text()')
-        item["price"]       = self.format_xpath(response, '//*[@id="inificha"]/div/div[2]/div[1]/div[1]/span/text()').split()[0]
-        item["update_date"] = self.format_xpath(response, '//*[@id="contents_n"]/div[4]/div/div[1]/span/text()').split()[4].replace("(","").replace(")","")
+        item["title"]       = self.format_xpath(response, '//*[@class="h1ficha"]/text()')
+        item["price"]       = self.format_xpath(response, '//*[@class="precio-ficha"]/span/text()').split()[0]
+        item["detailReference"] = self.format_xpath(response, '//*[@class="referencia-ficha"]/span/text()')
+        item["rooms"]       = ""
+        item["surface"]       = ""
+        #item["update_date"] = self.format_xpath(response, '//*[@id="contents_n"]/div[4]/div/div[1]/span/text()').split()[4].replace("(","").replace(")","")
         #item["rooms"]       = self.format_xpath(response, '//*[@class="detail-rooms"]/text()').split()[0]
         #item["surface"]     = self.format_xpath(response, '//*[@class="detail-m2"]/text()')[:-1]
         #item["location"]    = self.format_xpath(response, '/html/body/div[5]/span/text()')
